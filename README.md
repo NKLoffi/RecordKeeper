ce0cb8f95c883de59c440762cf72fe0c46bbbe80
NKLoffi <nklajiofficial@gmail.com>
2025-03-07 11:38:23 -0500

    Code optimization
    Optimized the code by using Dictionaries and for loops for a cleaner and more readable code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RecordKeeper
+
+**Record keeper is an application for entering the details of the user and keeping them in the Database Securely**
+
+Python is used for creating the application and for the GUI, PyQt is used
+
+To run the program you need to install:
+
+
--- Recordkeeper/main.py
+++ Recordkeeper/main.py
@@ -2,14 +2,13 @@ from database import Database
 import sys
 from PyQt5.QtWidgets import (QPushButton, QLineEdit, QApplication, QMainWindow,
                               QLabel, QVBoxLayout, QHBoxLayout, QWidget, QSizePolicy)
-from PyQt5.QtGui import QScreen, QIcon
-from PyQt5.QtCore import Qt, QPropertyAnimation, QSize
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()    # initalizing the main window
 
-        self.db = Database()  #initalizing database connection
+        self.db = Database()  #initalizing database connection 
         self.db.create_table() # To create table
 
         self.setWindowTitle("Information")  # setting title to the window
@@ -28,117 +27,69 @@ class MainWindow(QMainWindow):
         self.initUI()
 
 
-
         # Lets just make some text boxes for entering our data
 
     def initUI(self): # initalizing the UI
         central_widget = QWidget() 
         self.setCentralWidget(central_widget)
 
-        fname_label = QLabel("First Name: ")
-        lname_label = QLabel("Last Name: ")
-        email_label = QLabel("Email: ")
-        dob_label = QLabel("DOB: ")
-        sin_label = QLabel("SIN: ")
-        addy_label = QLabel("Address: ")
-        city_label = QLabel("City: ")
-        province_label = QLabel("Province")
+        self.labels = {                             # Stored the labels in Dictionary
+            "fname": QLabel("First name: "),
+            "lname": QLabel("Last Name: "),
+            "email": QLabel("Email: "),
+            "dob": QLabel("DOB: "),
+            "sin": QLabel("SIN: "),
+            "address" : QLabel("Address: "),
+            "city": QLabel("City: "),
+            "province": QLabel("Province")
 
-        self.fname_box = QLineEdit(self)
-        self.lname_box  = QLineEdit(self)
-        self.email_box = QLineEdit(self)
-        self.dob_box = QLineEdit(self)
-        self.sin_box = QLineEdit(self)
-        self.addy_box = QLineEdit(self)
-        self.city_box = QLineEdit(self)
-        self.province_box = QLineEdit(self)
+        }
 
+        self.text_boxes = {                     # Stored all the Text boxes in Dictionary
+            "fname": QLineEdit(self),
+            "lname": QLineEdit(self),
+            "email": QLineEdit(self),
+            "dob": QLineEdit(self),
+            "sin": QLineEdit(self),
+            "address": QLineEdit(self),
+            "city": QLineEdit(self),
+            "province": QLineEdit(self),
+        }
 
-        self.fname_box.setPlaceholderText("eg. John")
-        self.lname_box.setPlaceholderText("eg. Doe")
-        self.email_box.setPlaceholderText("eg. doejohn@gmail.com")
-        self.dob_box.setPlaceholderText("eg. DD-MM-YYYY")
-        self.sin_box.setPlaceholderText("eg. 000-000-000")
-        self.addy_box.setPlaceholderText("eg. 22 Devitt Ave N")
-        self.city_box.setPlaceholderText("eg. Waterloo")
-        self.province_box.setPlaceholderText("eg. Ontario")
+        placeholders ={                        # Stored all placeholders in Dictionary
+            "fname": "eg.John",
+            "lname": "eg. doe",
+            "email": "eg. doejohn@gmail.com",
+            "dob": "eg. DD-MM-YYY",
+            "sin": "eg. 000-000-000",
+            "address": "eg. 22 Devitt Ave N",
+            "city": "Waterloo",
+            "province": "Ontario",
+        }
+
+        for key, textbox in self.text_boxes.items():
+            textbox.setPlaceholderText(placeholders[key])
+            textbox.setMinimumWidth(200)
+            textbox.setAlignment(Qt.AlignLeft)
+            textbox.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
 
         layout = QVBoxLayout()
 
-        fname_layout = QHBoxLayout()
-        fname_layout.addWidget(fname_label)
-        fname_layout.addWidget(self.fname_box)
-
-        lname_layout = QHBoxLayout()
-        lname_layout.addWidget(lname_label)
-        lname_layout.addWidget(self.lname_box)
-
-        email_layout = QHBoxLayout()
-        email_layout.addWidget(email_label)
-        email_layout.addWidget(self.email_box)
-
-        dob_layout = QHBoxLayout()
-        dob_layout.addWidget(dob_label)
-        dob_layout.addWidget(self.dob_box)
-
-        sin_layout = QHBoxLayout()
-        sin_layout.addWidget(sin_label)
-        sin_layout.addWidget(self.sin_box)
-
-        addy_layout = QHBoxLayout()
-        addy_layout.addWidget(addy_label)
-        addy_layout.addWidget(self.addy_box)
-
-        city_layout = QHBoxLayout()
-        city_layout.addWidget(city_label)
-        city_layout.addWidget(self.city_box)
-
-        province_layout = QHBoxLayout()
-        province_layout.addWidget(province_label)
-        province_layout.addWidget(self.province_box)
+        for key in self.labels:
+            row_layout = QHBoxLayout()
+            row_layout.addWidget(self.labels[key])
+            self.labels[key].setFixedWidth(100)
+            row_layout.addWidget(self.text_boxes[key])
+            row_layout.addStretch()
+            layout.addLayout(row_layout)
 
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.submit_button)
-
-        layout.addLayout(fname_layout)
-        layout.addLayout(lname_layout)
-        layout.addLayout(email_layout)
-        layout.addLayout(dob_layout)
-        layout.addLayout(sin_layout)
-        layout.addLayout(addy_layout)
-        layout.addLayout(city_layout)
-        layout.addLayout(province_layout)
+        button_layout.addStretch()
         layout.addLayout(button_layout)
-
-
 
         central_widget.setLayout(layout)
         layout.setSpacing(10)
-
-
-
-        text_box = [self.fname_box, self.lname_box, self.email_box, self.dob_box, self.sin_box, self.addy_box, self.city_box, self.province_box]
-        for box in text_box:
-            box.setMinimumWidth(200)
-            box.setAlignment(Qt.AlignLeft)
-            box.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-
-        labels = [fname_label, lname_label, email_label, dob_label, sin_label, addy_label, city_label, province_label]
-        for label in labels:
-            label.setFixedWidth(100)
-
-
-        fname_layout.addStretch()
-        lname_layout.addStretch()
-        email_layout.addStretch()
-        dob_layout.addStretch()
-        sin_layout.addStretch()
-        addy_layout.addStretch()
-        city_layout.addStretch()
-        province_layout.addStretch()
-        button_layout.addStretch()
-
-
 
         self.submit_button.setObjectName("submitButton")
 
@@ -151,28 +102,33 @@ class MainWindow(QMainWindow):
             QPushButton#submitButton{
 
                 background-color: #4CAF50;
+                color: white;
 
                 }
                            
             
-            
             QLineEdit{
                 background-color: #3E4451;
+                border: solid black;
+                border-radius: 5px;
                 margin-left: 10px;
-
+                color: white;
                            }
 
-
+            QLabel{
+                color: white;
+                           }
         """)
     def submit_data(self):
-        first_name = self.fname_box.text()
-        last_name = self.lname_box.text()
-        email = self.email_box.text()
-        dob = self.dob_box.text()
-        sin = self.sin_box.text()
-        address = self.addy_box.text()
-        city = self.city_box.text()
-        province = self.province_box.text()
+
+        first_name = self.text_boxes["fname"].text()
+        last_name = self.text_boxes["lname"].text()
+        email = self.text_boxes["email"].text()
+        dob = self.text_boxes["dob"].text()
+        sin = self.text_boxes["sin"].text()
+        address = self.text_boxes["address"].text()
+        city = self.text_boxes["city"].text()
+        province = self.text_boxes["province"].text()
 
         self.db.insert_user_data(first_name, last_name, email, dob, sin, address, city, province)
 
