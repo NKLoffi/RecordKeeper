e903e0fb5e94ee9a4ea77872d1c73043bc38181d
NKLoffi <nklajiofficial@gmail.com>
2025-03-15 09:59:51 -0400

    Updated CSS properties
    updated the CSS properties for mainwindow, lineedit, and submit button. fixed the width, height and colour of all the widgets. Updated all the colours in HSL value (eg. hsl(122, 39%, 40%); )

--- Recordkeeper/main.py
+++ Recordkeeper/main.py
@@ -113,27 +113,37 @@ class MainWindow(QMainWindow):
         self.setStyleSheet("""
                            
             QMainWindow{
-                background-color: #282C34;
+                background-color: hsl(220, 13%, 16%)
             }
                            
             QPushButton#submitButton{
 
                 background-color: hsl(122, 39%, 49%);
                 color: white;
+                min-width: 100px;
+                max-width: 100px;
+                min-height: 25px;
+                max-height: 25px;
+                border-radius: 5px;
 
 
                 }
             QPushButton#submitButton:hover {
                            background-color: hsl(122, 39%, 40%);
+
                            
                            }
                            
             
             QLineEdit{
-                background-color: #3E4451;
+                background-color: hsl(221, 13%, 28%);
                 border: solid black;
                 border-radius: 5px;
                 margin-left: 10px;
+                min-width: 200px;
+                max-width: 200px;
+                min-height: 25px;
+                max-height: 25px;
                 color: white;
                            }
 
